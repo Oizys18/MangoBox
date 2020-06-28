e44340741805997fa9510e73016bd602dece7854
Oizys18 <oizys18@gmail.com>
2020-06-28 23:04:18 +0900

    Initial Commit - Create react app - Add typescript - Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import "./App.css";
+import logo from "./logo.svg";
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav>
+            <Link to="/">Home</Link>
+            <br></br>
+            <Link to="/intro">소개</Link>
+          </nav>
+          <Route exact={true} path="/" render={() => <h3>Home</h3>} />
+          <Route exact={true} path="/intro" render={() => <h3>소개</h3>} />
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
